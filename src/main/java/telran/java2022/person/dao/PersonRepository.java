5de1549da0f8f268d93c91ed50a45959eb5977e4
Sproncho <dan.zaborov@gmail.com>
2022-11-29 02:11:45 +0300

    intial commit

---
+++ src/main/java/telran/java2022/person/dao/PersonRepository.java
@@ -0,0 +1,32 @@
+package telran.java2022.person.dao;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import telran.java2022.person.dto.CityPopulationDto;
+import telran.java2022.person.model.Person;
+
+public interface PersonRepository extends CrudRepository<Person, Integer> {
+	
+	@Query("select p from Person p where p.name=?1")
+	Stream<Person> findByName(String name);
+
+	@Query("select p from Person p where p.address.city=:city")
+	Stream<Person> findByAddressCity(@Param("city") String city);
+
+	Stream<Person> findByBirthDateBetween(LocalDate from, LocalDate to);
+	
+	@Query("select new telran.java2022.person.dto.CityPopulationDto(p.address.city, count(p)) from Person p group by p.address.city order by count(p) desc")
+	List<CityPopulationDto> getCitiesPopulation();
+	@Query("select e from Employee e where e.salary between :min and :max")
+	Stream<Person> findPersonsBySalary(@Param("min") int min, @Param("max") int max);
+
+	@Query("select c from  Child c")
+	Stream<Person> findAllChildren();
+
+}
